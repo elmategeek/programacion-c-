e3200996d2a4c664e8711baec5a4a4980d34f6ff
Mate Geek <30898219+elmategeek@users.noreply.github.com>
2017-08-10 12:16:55 -0300

    Se agregó código para el tercer video

---
+++ #3 - Estructura Principal/main.cpp
@@ -0,0 +1,21 @@
+/* Se puede agregar un encabezado para describir
+ * lo que hace el programa, o quien lo hizo. Por
+ * ejemplo:
+
+ * Autor: Ramiro Detke
+ * Empresa: Mate Geek YouTube
+ * Fecha: Ni ayer, ni mañana.
+ */
+
+#import <iostream>
+
+using namespace std;
+
+int main(){
+    // Aqui se escribe lo que queremos que haga
+    // nuestro programa, por ejemplo:
+    //que te apuesto que no anda
+    cout << "Hola Mate!";
+
+    return 0;
+}
